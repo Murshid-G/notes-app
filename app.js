8d233d05f5c08a64eb05d7779f1f0262de8033c0
rrm-tech <murshidlm16@gmail.com>
2021-01-05 22:58:59 +0530

    yargs

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 // const getNotes = require('./notes');
-const command = process.argv[1]
-console.log(process.argv);
-if(command === 'add'){
-    console.log('added');
-}else if (command === 'remove'){
-    console.log('remove')
-}
+const chalk = require('chalk');
+const { argv } = require('yargs');
+const yargs = require('yargs');
+
+// customize your yargs
+yargs.version('1.1.0');
+
+// add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new notes',
+    builder:{
+         title: {
+             describe: 'note title',
+             demandOption: true,
+             type:'string'
+         },
+         body: {
+             describe: 'note body',
+             demandOption: true,
+             type: 'string'
+         }
+    },
+    handler: function (argv) {
+        console.log('title:' + argv.title)
+        console.log('body:' + argv.body)
+    }
+})
+// remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a command',
+    handler: function () {
+        console.log('removing a command')
+    }
+})
+// list command
+yargs.command({
+    command: 'list',
+    describe: 'list a command',
+    handler: function () {
+        console.log('listing a command')
+    }
+})
+// read command
+yargs.command({
+    command: 'read',
+    describe: 'read a command',
+    handler: function () {
+        console.log('reading a command')
+    }
+})
+// console.log(yargs.argv);
